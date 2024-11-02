1ad495d6c32f5545840dfa1f66dc2d780891bf40
LUORANCHENG <2106790264@qq.com>
2024-11-02 11:37:30 +0800

    update_video2img.py

--- video2img.py
+++ video2img.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 
-def video_to_images(video_path, output_dir):
+def video_to_images(video_path, output_dir, scale_factor_):
     # 检查输出目录是否存在，不存在则创建
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -16,7 +16,7 @@ def video_to_images(video_path, output_dir):
 
     frame_count = 0  # 视频帧计数
     save_count = 1   # 保存图片的计数，用于编号
-    scale_factor = 0.25  # 缩小4倍
+    scale_factor_img = scale_factor_  # 缩小4倍
 
     while True:
         # 读取视频帧
@@ -28,7 +28,7 @@ def video_to_images(video_path, output_dir):
         if frame_count % 20 == 0:
             # 缩小图像
             height, width = frame.shape[:2]
-            new_size = (int(width * scale_factor), int(height * scale_factor))
+            new_size = (int(width * scale_factor_img), int(height * scale_factor_img))
             resized_frame = cv2.resize(frame, new_size, interpolation=cv2.INTER_AREA)
 
             # 保存图像，编号格式为 0001, 0002, ...
@@ -48,9 +48,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_path", default="", type=str, help="video_path")
     parser.add_argument("--output_dir", default="", type=str, help="output_dir")
+    parser.add_argument("--scale_factor", default="", type=float, help="output_dir")
     args = parser.parse_args()
 
     # 使用示例
     video_path = args.video_path
     output_dir = args.output_dir
-    video_to_images(video_path, output_dir)
+    scale_factor = args.scale_factor
+    video_to_images(video_path, output_dir, scale_factor)
